4b3179d6c2896f732b149cbcb219d45b6ba80f78
anto1282 <123944428+anto1282@users.noreply.github.com>
2024-10-09 16:51:57 +0200

    Update DataLoader_PH2.py
    Added Imports

--- DataLoader_PH2.py
+++ DataLoader_PH2.py
@@ -1,3 +1,23 @@
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+
+# pip install torchsummary
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision import models
+from torchsummary import summary
+import torch.optim as optim
+from time import time
+
+
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
 data_path = '/dtu/datasets1/02516/PH2_Dataset_images/'
 class PH2(torch.utils.data.Dataset):
     def __init__(self, transform, data_path=data_path):
