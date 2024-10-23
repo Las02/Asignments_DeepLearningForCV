45c8a5de0cfb192751b85553fea98e07f9a7a677
anto1282 <s203557@dtu.dk>
2024-10-23 15:42:48 +0200

    godt  shit

--- DataLoader_PH2_weakpoints.py
+++ DataLoader_PH2_weakpoints.py
@@ -10,22 +10,29 @@ import os
 data_path = '/dtu/datasets1/02516/PH2_Dataset_images/'
 class PH2_weekpoints(torch.utils.data.Dataset):
     
-    def __init__(self, transform, number_of_points, data_path=data_path):
+    def __init__(self, transform, number_of_points,train,  data_path=data_path):
         'Initialization'
+        self.train = train
         self.number_of_points = number_of_points
         self.transform = transform
         self.image_paths = []
         self.label_paths = []
+        
         for folder in os.listdir(data_path):
             self.image_paths.append(data_path + f"{folder}/{folder}_Dermoscopic_Image/{folder}.bmp")
             self.label_paths.append(data_path + f"{folder}/{folder}_lesion/{folder}_lesion.bmp")
-        
+        if train:
+            self.image_paths = self.image_paths[0:180]
+            self.label_paths = self.label_paths[0:180]
+        else:
+            self.image_paths = self.image_paths[180:]
+            self.label_paths = self.label_paths[180:]
     def __len__(self):
         'Returns the total number of samples'
         return len(self.image_paths)
     
     def __getitem__(self, idx):
-        'Generates one sample of data'
+        'Generates one sample of data' 
         image_path = self.image_paths[idx]
         label_path = self.label_paths[idx]
         
@@ -33,21 +40,21 @@ class PH2_weekpoints(torch.utils.data.Dataset):
         label = Image.open(label_path)
         Y = self.transform(label)
         X = self.transform(image)
+        if self.train:
+            ones_indices = (Y == 1).nonzero(as_tuple=True)
+            zeros_indices = (Y == 0).nonzero(as_tuple=True)
+
+            # Randomly select 10 indices from 1s and 10 from 0s to preserve
+            preserve_ones = torch.randperm(len(ones_indices[0]))[:self.number_of_points]
+            preserve_zeros = torch.randperm(len(zeros_indices[0]))[:self.number_of_points]
+
+            # Create a mask to set everything to 0.5 except the preserved ones and zeros
+            Y = torch.ones_like(Y) * 0.5
+
+            # Set the preserved indices back to their original values
+            Y[ones_indices[0][preserve_ones], ones_indices[1][preserve_ones], ones_indices[2][preserve_ones]] = 1
+            Y[zeros_indices[0][preserve_zeros], zeros_indices[1][preserve_zeros], zeros_indices[2][preserve_zeros]] = 0
         
-        ones_indices = (Y == 1).nonzero(as_tuple=True)
-        zeros_indices = (Y == 0).nonzero(as_tuple=True)
-
-        # Randomly select 10 indices from 1s and 10 from 0s to preserve
-        preserve_ones = torch.randperm(len(ones_indices[0]))[:self.number_of_points]
-        preserve_zeros = torch.randperm(len(zeros_indices[0]))[:self.number_of_points]
-
-        # Create a mask to set everything to 0.5 except the preserved ones and zeros
-        Y = torch.ones_like(Y) * 0.5
-
-        # Set the preserved indices back to their original values
-        Y[ones_indices[0][preserve_ones], ones_indices[1][preserve_ones], ones_indices[2][preserve_ones]] = 1
-        Y[zeros_indices[0][preserve_zeros], zeros_indices[1][preserve_zeros], zeros_indices[2][preserve_zeros]] = 0
-
         
         return X, Y
 
